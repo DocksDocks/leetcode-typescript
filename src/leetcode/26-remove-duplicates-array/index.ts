cc3c6c52ad4f71347d9fb34adb5b7bf0a901c3c5
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2025-06-05 00:15:29 -0300

    26 - remove duplicates

---
+++ src/leetcode/26-remove-duplicates-array/index.ts
@@ -0,0 +1,21 @@
+// src/leetcode/26-remove-duplicates-array/index.ts
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+function removeDuplicates(nums: number[]): number {
+    let t: number[] = [];
+    nums.forEach(n => {
+        const alreadyInT = t.includes(n);
+        if (!alreadyInT) {
+            t.push(n)
+        }
+    })
+    nums.splice(0, nums.length, ...t)
+    return nums.length
+};
+
+export default function main() {
+    let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+    removeDuplicates(nums);
+    console.log({ nums })
+}
+
+main()
