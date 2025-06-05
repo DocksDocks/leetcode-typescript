f9dffdeb1864b0d9a31229f40cd191967021320f
DocksDocks <55303379+DocksDocks@users.noreply.github.com>
2025-06-04 21:25:38 -0300

    remove element challenge

--- src/leetcode/27-remove-element/index.ts
+++ src/leetcode/27-remove-element/index.ts
@@ -1,7 +1,7 @@
 // src/leetcode/27-remove-element/index.ts
 // https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
 
-export function removeElement(nums: number[], val: number): number {
+function removeElement(nums: number[], val: number): number {
     let filteredNums: number[] = [];
     nums.forEach((value) => {
         if (value !== val) {
@@ -11,3 +11,11 @@ export function removeElement(nums: number[], val: number): number {
     nums.splice(0, nums.length, ...filteredNums);
     return nums.length;
 };
+
+export default function main() {
+    let nums = [3, 2, 2, 3], val = 3;
+    const k = removeElement(nums, val);
+    console.log({ nums, k })
+}
+
+main()
